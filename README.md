64316393aa66ae453fd92d2a8d3a22b78fdddcb3
cristian.passos <cristian.passos@sp.agence.com.br>
2022-11-08 21:25:40 -0400

    Formulario com modal

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 # instruções
 
 https://docs.google.com/document/d/16WETFtNOwNKJZj-ySvMutV769lQV1joHdnFs079M61o/edit
+
+// nome ok
+
+// especie 
+// tipo
+ 
+// habilidade
+// fraqueza
+// evolução
---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Get the modal
+let modal = document.getElementById("modalPokemon");
+
+// Get the button that opens the modal
+let btn = document.getElementById("capturarPokemon");
+
+// mostrar o modal
+btn.addEventListener('click', function (evento){
+  modal.style.display = "block"
+});
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
