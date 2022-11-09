143359ccaa2ed6190fe65470bc49527a4a97137a
cristian.passos <cristian.passos@sp.agence.com.br>
2022-11-09 16:23:36 -0400

    Melhorias do formulario

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ let nomePokemon = document.getElementById("nome");
 let tipoPokemon = document.getElementById("listaTipos");
 let imagemPokemon = document.getElementById("imagem");
 const formulario = document.getElementById("formulario");
+let imagemLoad = document.getElementById("imagem_load");
 
 
 // mostrar o modal
@@ -25,25 +26,56 @@ span.onclick = function () {
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function (event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+    
+//   }
+// }
 
 // ---------- validação do form --------
 function validate(){
   if(nomePokemon.value != "" && tipoPokemon.value != "" && imagemPokemon.value != ""){
     btnAddCard.disabled = false;
+    imagemLoad.src = imagemPokemon.value;
+
   }else{
+    if(imagemPokemon.value != ""){
+      imagemLoad.src = imagemPokemon.value;
+    }
     btnAddCard.disabled = true;
   }
 }
 
 // -------------- adiciona cards ------------------ 
 
+
+let pokemons = [];
+
 formulario.addEventListener('submit', e =>{
     e.preventDefault();
 
-})
+    let pokemon = {
+      "nome": nomePokemon.value,
+      "tipo": tipoPokemon.value,
+      "imagem": imagemPokemon.value
+    }
+    
+    pokemons.push(pokemon);
+
+    adicionarCard();
+    
+});
+
+
+function adicionarCard(){
+
+}
+
+
+// --------------- tratamento de imagem
+
+imagemLoad.onerror = function(){
+  imagemLoad.src = "https://thumbs.gfycat.com/EachWellinformedAidi-size_restricted.gif"
+}
 
